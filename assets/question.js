d76ffa8d2c562f9e629a042f26bdb9f9e55cc956
Pilar1936 <tesoro4me@hotmail.com>
2023-11-27 17:35:40 -0600

    homework

---
+++ assets/question.js
@@ -0,0 +1,72 @@
+// questions, choices, and answers
+var questions = [
+  
+    {
+      title: "Inside which HTML element do we put the JavaScript?",
+      choices: [
+        "<script>",
+        "<javascript>",
+        "<scripjavas>",
+        "all of the above"
+      ],
+      answer: "<script>"
+    },
+    {
+      title: "Which one is a looping structure in JavaScript?",
+      choices: ["All the below", "For", "While", "do-while loops"],
+      answer: "All the below"
+    },
+    {
+      title: "How do you create a function in JavaScript?",
+      choices: [
+        "function=myfunction()",
+        "function myfunction()",
+        "None of the above"
+      ],
+      answer: "function myfunction()"
+    },
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+    },
+    {
+      title: "Boolean operators that can be used in JavaScript include:",
+      choices: [
+        "'And' Operator &&",
+        "'Or' Operator ||",
+        "'Not' Operator !",
+        "All the above"
+      ],
+      answer: "All the above"
+    },
+   
+    {
+      title: "What is the data type of variables in JavaScript?",
+      choices: [
+        "Object data types",
+        "Function data type",
+        "None of the above",
+        "All of the above"
+      ],
+      answer: "Object data types"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+    },
+    {
+      title:
+        "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parentheses"],
+      answer: "quotes"
+    },
+    {
+      title:
+        "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      answer: "console.log"
+    },
+  
+  ];
